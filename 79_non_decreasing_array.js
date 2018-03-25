5da69bcadbd07ab5277d4bcf2a53317ca351212c
Samu Jukkola <samu.jukkola@almamedia.fi>
2018-03-25 19:26:06 +0300

    add package.json & Mocha test-runner

--- 79_non_decreasing_array.js
+++ 79_non_decreasing_array.js
@@ -9,11 +9,14 @@ Given the array [10, 5, 1], you should return false,
 since we can't modify any one element to get a non-decreasing array.
 */
 "use strict"
+const assert = require('assert');
+const path = require('path');
+
 const notFirstOrLast = (i, arr) => i > 0 && i + 2 < arr.length;
 
 function check(arr) {
   let timesModified = 0;
-  for (let i = 0; i < arr.length; i++){
+  for (let i = 0; i < arr.length; i++) {
     if (arr[i] > arr[i + 1]) {
       if (timesModified === 1) return false;
 
@@ -29,12 +32,23 @@ function check(arr) {
   return true;
 }
 
-console.log(check([10, 5, 7])); // true
-console.log(check([10, 5, 1])); // false
-console.log(check([1, 1, 1, 2, 3, 1, 2])); // false
-console.log(check([1, 1, 1, 2, 3, 2])); // true
-console.log(check([1, 1, 1, 2, 3, 2, 2, 3, 2])); // false
-console.log(check([2, 1, 1, 2, 2, 2, 3, 2])); // false
+if (global.describe !== 'function') {
+  console.log('Please run with Mocha test-runner:');
+  console.log('\x1b[33m%s\x1b[0m', `  - mocha ${path.basename(__filename)}`);
+  process.exit(1);
+}
 
+describe('Test', () => {
+  it('is decreasing array', function () {
+    assert.equal(check([10, 5, 7]), true);
+    assert.equal(check([1, 1, 1, 2, 3, 2]), true);
+    assert.equal(check([1, 2, 1, 2, 2, 2, 3]), true);
+  });
 
-
+  it('is non-decreasing array', function () {
+    assert.equal(check([10, 5, 1]), false);
+    assert.equal(check([1, 2, 1, 2, 2, 1, 2]), false);
+    assert.equal(check([1, 1, 1, 2, 3, 2, 2, 3, 2]), false);
+    assert.equal(check([2, 1, 1, 2, 2, 2, 3, 2]), false);
+  });
+});
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # My daily coding problems
-Run with Node (v8.1.3)
+Run with Node (v8.1.3) or Mocha test-runner
+
+`yarn install`
 
 [from dailycodingproblem.com mailing list](https://www.dailycodingproblem.com)
 
