60bc556f4dbe95b3c1ce96621c689d4966d81fc8
Samu Jukkola <samu.jukkola@solita.fi>
2025-11-18 14:52:50 +0200

    Convert to typescript & add tests

--- 401_permutation_by_array.ts
+++ 401_permutation_by_array.ts
@@ -1,23 +1,13 @@
 /*
-A permutation can be specified by an array P,
-where P[i] represents the location of the element at i in the permutation.
-For example, [2, 1, 0] represents the permutation where elements at the index 0 and 2 are swapped.
+ * A permutation can be specified by an array P,
+ * where P[i] represents the location of the element at i in the permutation.
+ * For example, [2, 1, 0] represents the permutation where elements at the index 0 and 2 are swapped.
+ *
+ * Given an array and a permutation, apply the permutation to the array.
+ * For example, given the array ["a", "b", "c"] and the permutation [2, 1, 0],
+ * return ["c", "b", "a"].
+ */
 
-Given an array and a permutation, apply the permutation to the array.
-For example, given the array ["a", "b", "c"] and the permutation [2, 1, 0],
-return ["c", "b", "a"].
-*/
-
-"use strict"
-const assert = require('assert');
-
-function permutation(array, permutation) {
-  return permutation.map(p => array[p]);
+export const applyPermutation = <T>(array: readonly T[], permutation: readonly number[]): T[] => {
+  return permutation.map((index) => array[index])
 }
-
-describe('Test', () => {
-  it('Orders by frequency', function () {
-    assert.deepEqual(permutation(["a", "b", "c"], [2, 1, 0]), ["c", "b", "a"]);
-  });
-});
-
